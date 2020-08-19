c242678da0b38fd482d5ef7e71e71d907cea41f2
Brian Icaro D. Marques <brian.icaro.dm@gmail.com>
2020-08-19 19:06:58 -0300

    finished the modal example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import Modal from './Modal';
 
-function App() {
+const App = () => {
+  const modalRef = useRef();
+
+  const openModal = () => {
+    modalRef.current.openModal();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={openModal}>Open Modal</button>
+      <Modal ref={modalRef}>
+        <h1>Modal Header</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mussum Ipsum, cacilds vidis litro abertis. Nullam volutpat risus nec
+          leo commodo, ut interdum diam laoreet. Sed non consequat odio. Nec
+          orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non
+          ipsum felis. Pra lá , depois divoltis porris, paradis. Interagi no mé,
+          cursus quis, vehicula ac nisi. Copo furadis é disculpa de bebadis,
+          arcu quam euismod magna. Não sou faixa preta cumpadi, sou preto
+          inteiris, inteiris. Leite de capivaris, leite de mula manquis sem
+          cabeça. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua
+          vidis. Aenean aliquam molestie leo, vitae iaculis nisl. Mé faiz
+          elementum girarzis, nisi eros vermeio. Suco de cevadiss, é um leite
+          divinis, qui tem lupuliz, matis, aguis e fermentis. Per aumento de
+          cachacis, eu reclamis.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
